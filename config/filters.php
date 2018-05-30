a67e7e87e4b38ee16dee3d9a4c3db44e396d8183
Tomasz Chmielewski <chmielewski.thomas@gmail.com>
2018-05-30 18:30:30 +0200

    ArrayKeys

--- config/filters.php
+++ config/filters.php
@@ -3,5 +3,6 @@
 declare(strict_types = 1);
 
 return [
-    'Sanitize' => \Hop\Validator\Filter\Sanitize::class
+    'Sanitize' => \Hop\Validator\Filter\Sanitize::class,
+    'ArrayKeys' => \Hop\Validator\Filter\ArrayKeys::class,
 ];
